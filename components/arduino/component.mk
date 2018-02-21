0d82a5921c7abb9e95eddf340408bb62fa0e30f6
mfp20 <20516565+mfp20@users.noreply.github.com>
2018-02-21 16:40:10 +0100

    Removed: - components/arduino/sources git submodule - component/arduino directory

--- components/arduino/component.mk
+++
@@ -1,8 +0,0 @@
-$(call compile_only_if,$(CONFIG_COMPONENT_ARDUINO),*.o)
-
-ARDUINO_CORE_LIBS := $(patsubst $(COMPONENT_PATH)/sources/%,%,$(sort $(dir $(wildcard $(COMPONENT_PATH)/sources/libraries/*/*/))))
-
-COMPONENT_ADD_INCLUDEDIRS := sources/cores/esp32 sources/variants/esp32 $(ARDUINO_CORE_LIBS)
-COMPONENT_PRIV_INCLUDEDIRS := sources/cores/esp32/libb64
-COMPONENT_SRCDIRS := sources/cores/esp32/libb64 sources/cores/esp32 sources/variants/esp32 $(ARDUINO_CORE_LIBS)
-CXXFLAGS += -fno-rtti
